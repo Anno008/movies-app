ecb8cf8c6efae6c5eb29598119b038e54ca1b40a
Aleksa Jovicic <aleksa.j@outlook.com>
2021-07-06 19:57:31 +0200

    Mock fetch

--- src/testUtils/setupTests.ts
+++ src/testUtils/setupTests.ts
@@ -1,1 +1,23 @@
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
+
+// Global fetch mock as to prevent real fetch calls during test runs
+// for individual test cases where we want to assert the behavior depending
+// on the response data it is necessary to manually mock fetch per test unit
+// Example:
+// const fetchMock = jest
+//    .spyOn(global, "fetch")
+//    .mockImplementation(() => Promise.resolve({ json: () => Promise.resolve("What we want to receive") }));
+
+// const result = await functionThatCallsFetch();
+// expect(fetchMock).toHaveBeenCalledWith("url");
+
+beforeAll(() => {
+  global.fetch = () =>
+    Promise.resolve({
+      ...global.fetch.prototype,
+      body: false,
+      status: 200,
+      json: () => Promise.resolve(),
+      arrayBuffer: () => Promise.resolve()
+    });
+});
