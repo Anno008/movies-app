314ce066fddbe857ac4ccd335e8fe5d8cfc6da96
Aleksa Jovicic <aleksa.j@outlook.com>
2021-07-06 19:59:24 +0200

    Create type definitions

---
+++ src/types/MoviesData.ts
@@ -0,0 +1,13 @@
+import { Movie } from "./Movie";
+
+// Envelope
+export interface MoviesData {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
