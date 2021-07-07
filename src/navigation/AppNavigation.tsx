87016eaeb955326dfe0f07832342c3d266d55e22
Aleksa Jovicic <aleksa.j@outlook.com>
2021-07-07 13:47:17 +0200

    Style app navigation

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -2,19 +2,33 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import { useTheme } from "styled-components";
+
 import MoviesScreen from "screens/MoviesScreen";
 
 const Stack = createStackNavigator();
 
-const AppNavigation = (): JSX.Element => (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Movies"
-        component={MoviesScreen}
-        options={{ title: "Upcoming movies" }}
-      />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+const AppNavigation = (): JSX.Element => {
+  const theme = useTheme();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: theme.secondaryBackgroundColor,
+            elevation: 0,
+            shadowOpacity: 0
+          },
+          headerTintColor: theme.primaryTextColor
+        }}>
+        <Stack.Screen
+          name="Movies"
+          component={MoviesScreen}
+          options={{ title: "Upcoming movies" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 export default AppNavigation;
