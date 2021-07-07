a3bb0683f0ac1e78a885eca763f887f99a410ab4
Aleksa Jovicic <aleksa.j@outlook.com>
2021-07-07 13:29:49 +0200

    Wrap app in ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
 import AppNavigation from "navigation/AppNavigation";
+import { DarkTheme } from "theme/DarkTheme";
 
-const App = (): JSX.Element => <AppNavigation />;
+const App = (): JSX.Element => (
+  <ThemeProvider theme={DarkTheme}>
+    <AppNavigation />
+  </ThemeProvider>
+);
 
 export default App;
