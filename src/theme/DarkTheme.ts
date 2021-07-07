996348d1afdcc132a6b6ecb8da129d78c090950d
Aleksa Jovicic <aleksa.j@outlook.com>
2021-07-07 13:29:29 +0200

    Create styled components theme file, and define it's typescript definition

---
+++ src/theme/DarkTheme.ts
@@ -0,0 +1,9 @@
+import * as StyledComponents from "styled-components";
+
+const DarkTheme: StyledComponents.DefaultTheme = {
+  primaryTextColor: "#FFF",
+  primaryBackgroundColor: "#242526",
+  secondaryBackgroundColor: "#131414"
+};
+
+export { DarkTheme };
---
+++ src/theme/styled.d.ts
@@ -0,0 +1,17 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    primaryTextColor: string;
+    primaryBackgroundColor: string;
+    secondaryBackgroundColor: string;
+  }
+
+  declare interface StyledFlatList {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    FlatList<T>(styles: any): new () => ReactNative.FlatList<T>;
+  }
+
+  declare const styled: ReactNativeStyledInterface<DefaultTheme> &
+    StyledFlatList;
+}
