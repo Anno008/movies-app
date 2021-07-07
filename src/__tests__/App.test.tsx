b586af797a717222bae1834960c691987465aae2
Aleksa Jovicic <aleksa.j@outlook.com>
2021-07-07 13:47:47 +0200

    App tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-
 import { render } from "@testing-library/react-native";
+
+import locators from "testUtils/locators";
 
 import App from "../App";
 
 describe("App tests", () => {
-  it("Should display movies screen on app boot", () => {
+  it("Should display movies screen on app boot", async () => {
     const renderResult = render(<App />);
-    const title = renderResult.getByText("Movies Screen");
-    expect(title).toBeDefined();
+
+    const list = await renderResult.findByTestId(locators.moviesScreenList);
+    expect(list).toBeDefined();
   });
 });
