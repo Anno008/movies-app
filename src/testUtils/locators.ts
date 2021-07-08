cb98aad7dbf0caab0513e5808debb2709bdeb4b9
Aleksa Jovicic <aleksa.j@outlook.com>
2021-07-08 12:03:25 +0200

    Locators

--- src/testUtils/locators.ts
+++ src/testUtils/locators.ts
@@ -1,4 +1,7 @@
 export default {
   moviesScreenList: "movies-screen-list",
-  movieCardImage: "movie-card-image"
+  movieCardImage: "movie-card-image",
+  searchMoviesNavigationButton: "search-movies-navigation-button",
+  movieCardContainer: "movie-card-container",
+  searchMoviesTextInput: "search-movies-text-input"
 };
