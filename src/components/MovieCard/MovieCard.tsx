1e319d293ccb12a484d78e60ba13d385748ed8af
Aleksa Jovicic <aleksa.j@outlook.com>
2021-07-08 12:09:11 +0200

    Change the layout of movie card

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useTheme } from "styled-components";
-import { Dimensions } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import {
   Grid,
@@ -12,38 +12,57 @@ import { Movie } from "types";
 import { imageEndpoint } from "constants/config";
 import { setTestId } from "testUtils/setTestId";
 import locators from "testUtils/locators";
+import { deviceWidth } from "device/getSize";
+import { GenreContext } from "contexts/GenreContext/GenreContext";
 
-const MovieCard = (movie: Movie): JSX.Element => {
+const MovieCard = ({
+  id,
+  title,
+  release_date,
+  backdrop_path,
+  genre_ids,
+  poster_path
+}: Movie): JSX.Element => {
   const theme = useTheme();
-  const imageWidth = Math.ceil(Dimensions.get("window").width / 100) * 100;
+  const genres = useContext(GenreContext);
+  const imageWidth = Math.ceil(deviceWidth / 100) * 100;
+  const combinedGenreNames = genres
+    .filter(genre => genre_ids.includes(genre.id))
+    .map(x => x.name)
+    .join(", ");
 
+  const { navigate } = useNavigation();
   return (
     <TouchableOpacity
-      margin={10}
+      marginVertical={10}
       borderRadius="10px"
+      {...setTestId(`${locators.movieCardContainer}-${id}`)}
+      onPress={() =>
+        navigate("MovieDetails", {
+          id
+        })
+      }
       border={`1px solid ${theme.secondaryBackgroundColor}`}
       overflow="hidden">
       <ImageBackground
-        elWidth="100%"
         height={200}
         {...setTestId(locators.movieCardImage)}
-        alignItems="flex-start"
-        justifyContent="flex-start"
         source={{
-          uri: `${imageEndpoint}w${imageWidth}/${
-            movie.backdrop_path || movie.poster_path
-          }`
+          uri: `${imageEndpoint}w${imageWidth}/${backdrop_path || poster_path}`
         }}>
         <Grid
           margin={10}
           backgroundColor="transparent"
           flexStretch="1"
           justifyContent="space-between">
-          <Typography bold fontSize="20px">
-            {movie.title}
-          </Typography>
-          <Typography bold fontSize="20px">
-            Rating: {movie.vote_average} / 10
+          <Grid backgroundColor="transparent">
+            <Typography bold fontSize="20px" textShadow>
+              {title}
+            </Typography>
+            <Typography fontSize="16px">{combinedGenreNames}</Typography>
+          </Grid>
+          <Typography bold fontSize="20px" alignSelf="flex-end">
+            {release_date}
           </Typography>
         </Grid>
       </ImageBackground>
