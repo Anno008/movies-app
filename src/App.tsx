87f2c4dc448986e064d98cb98dd5b7fde214cc8c
Aleksa Jovicic <aleksa.j@outlook.com>
2021-07-08 12:10:42 +0200

    Wrap app with providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import Toast from "react-native-toast-message";
 
-import AppNavigation from "navigation/AppNavigation";
+import GenreProvider from "contexts/GenreContext/GenreProvider";
 import { DarkTheme } from "theme/DarkTheme";
+import AppNavigation from "navigation/AppNavigation";
 
+// Bootstrapper
 const App = (): JSX.Element => (
-  <ThemeProvider theme={DarkTheme}>
-    <AppNavigation />
-  </ThemeProvider>
+  <GenreProvider>
+    <ThemeProvider theme={DarkTheme}>
+      <AppNavigation />
+      <Toast ref={ref => Toast.setRef(ref)} />
+    </ThemeProvider>
+  </GenreProvider>
 );
 
 export default App;
